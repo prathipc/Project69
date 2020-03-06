b48db6f3c6bc143d512e0a1e49bf90f3d1cbe173
Prathipkumar Chakravarthy <prathipc@yahoo.com?>
2020-03-06 15:11:08 -0800

    Just created the file

---
+++ roe.py
@@ -0,0 +1,12 @@
+import re
+import pandas
+
+
+class stock():
+    def __init__(self, l_stockname):
+        self.stockname = l_stockname
+        self.companyname = ""
+        self.industry = ""
+
+
+
